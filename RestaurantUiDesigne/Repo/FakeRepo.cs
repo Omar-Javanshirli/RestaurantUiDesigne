a1bfd273ac15ad5184d3bc20f19fb7718b92e1c6
Omer Cavanshirli <omrcvnsrli@hotmail.com>
2022-09-13 02:52:50 +0400

    new Update

---
+++ RestaurantUiDesigne/Repo/FakeRepo.cs
@@ -0,0 +1,27 @@
+﻿using RestaurantUiDesigne.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace RestaurantUiDesigne.Repo
+{
+    public class FakeRepo
+    {
+        public List<ImageClass> GetAllImage()
+        {
+            return new List<ImageClass>
+            {
+                new ImageClass
+                {
+                    Image="/Image/home2.png",
+                },
+                new ImageClass
+                {
+                    Image="/Image/pizza.png"
+                }
+            };
+        } 
+    }
+}
